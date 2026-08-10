f8f23cfc9cdd35b196cf47124d833e67850f22b5
Charles Villalpando <iroh-thinkpad@General-Iroh.attlocal.net>
2026-08-10 13:10:58 -0700

    Updated query fields

--- Routers/games.js
+++ Routers/games.js
@@ -12,7 +12,7 @@ gamesRouter.get("/featured", async (req, res, next) => {
   try {
     const featuredData = await fetchGameData(
       req.token,
-      `fields id,name,rating,cover.*,url; where rating > 75 & first_release_date > ${timeWindow}; sort rating desc;`,
+      `fields id,name,slug,rating,cover.*,url, first_release_date; where rating > 75 & first_release_date > ${timeWindow}; sort rating desc;`,
     );
 
     return res.status(200).json(featuredData);
@@ -26,7 +26,7 @@ gamesRouter.get("/goat", async (req, res, next) => {
   try {
     const greatestData = await fetchGameData(
       req.token,
-      `fields name,rating,cover.*,url; limit 20; sort rating_count desc;`,
+      `fields id,name,rating,cover.*,url; limit 20; sort rating_count desc;`,
     );
     return res.status(200).json(greatestData);
   } catch (err) {
@@ -41,7 +41,7 @@ gamesRouter.get("/goat", async (req, res, next) => {
 //   try {
 //     const searchQuery = await fetchGameData(
 //       req.token,
-//       `search "${query}"; fields name,release_dates.human;`,
+//       `search "${query}"; fields id,name,slug,rating,cover.*,first_release_date,url; where rating > 75 & first_release_date > ${timeWindow}; sort rating desc;`,
 //     );
 //     return res.status(200).json(searchQuery);
 //   } catch (err) {
